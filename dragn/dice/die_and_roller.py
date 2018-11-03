4faf1b10bfc18a5de7419f467bee3839baa21823
Gil Gonçalves <lursty@gmail.com>
2018-11-03 08:50:06 +0000

    Added mypy and type annotations

--- dragn/dice/die_and_roller.py
+++ dragn/dice/die_and_roller.py
@@ -1,14 +1,15 @@
 import random
+from typing import Any, Callable
 
 
 class Die:
-    def __init__(self, max_value, randomness):
+    def __init__(self, max_value: int, randomness: Callable) -> None:
         self.randomness = randomness
         self.max_value = max_value
 
-    def __call__(self):
+    def __call__(self) -> Any:
         return self.randomness(1, self.max_value)
 
 
-def roller(max_value, randomness=random.randint):
+def roller(max_value: int, randomness: Callable = random.randint) -> Any:
     return Die(max_value, randomness)()
