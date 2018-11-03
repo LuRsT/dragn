1f24d0a610f5fd80cfacf2b4a464298cc22ad028
Gil Gonçalves <lursty@gmail.com>
2018-11-03 08:50:06 +0000

    Added mypy and type annotations

--- dragn/tests/test_dice.py
+++ dragn/tests/test_dice.py
@@ -1,34 +1,36 @@
 from functools import partial
+from typing import Callable
 
 from dragn.dice import D4, D6, D8, D10, D12, D20
 from dragn.dice.die_and_roller import roller
 
 
+# mypy: ignore
 class TestRoller:
-    def test_roller_very_naive(self):
-        def fake_randomness(max_value, randomness):
+    def test_roller_very_naive(self) -> None:
+        def fake_randomness(max_value: int, randomness: Callable) -> int:
             return 4
 
         result = roller(0, randomness=fake_randomness)
 
         assert result == 4
 
-    def test_default_roller(self):
+    def test_default_roller(self) -> None:
         result = roller(6)
 
         assert result in range(1, 7)
 
-    def test_custom_roller_with_partial(self):
+    def test_custom_roller_with_partial(self) -> None:
         die = partial(roller, 6)
 
         assert die() in range(1, 7)
 
 
 class TestDie:
-    def test_one_configured_die(self):
+    def test_one_configured_die(self) -> None:
         assert D4() in range(1, 5)
 
-    def test_all_configured_dice(self):
+    def test_all_configured_dice(self) -> None:
         configured_dice = {
             D4: range(1, 5),
             D6: range(1, 7),
