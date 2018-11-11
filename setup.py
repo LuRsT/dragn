67b6d3d5280204e9147eb061805d3c47c9b7fe09
Gil Gonçalves <lursty@gmail.com>
2018-11-11 09:40:44 +0000

    Bump to version 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     # Project
     name="dragn",
-    version="0.0.5",
+    version="0.0.6",
     description="A library to emulate rolling dice",
     long_description=long_description,
     long_description_content_type="text/markdown",
