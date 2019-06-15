8c365ca283e5c2f12a4fb827a69c34440b9daf4f
Gil Gonçalves <lursty@gmail.com>
2019-06-15 22:49:05 +0100

    Make all tests statimethods, because they don't need `self`

--- dragn/tests/test_dice.py
+++ dragn/tests/test_dice.py
@@ -6,17 +6,20 @@ from dragn.dice.die import DieBuilder
 
 
 class TestDieBuilder:
-    def test_creating_one_die(self) -> None:
+    @staticmethod
+    def test_creating_one_die() -> None:
         my_new_die = DieBuilder(4)
 
         assert my_new_die() in range(1, 5)
 
 
 class TestDie:
-    def test_one_configured_die(self) -> None:
+    @staticmethod
+    def test_one_configured_die() -> None:
         assert D4() in range(1, 5)
 
-    def test_all_configured_dice(self) -> None:
+    @staticmethod
+    def test_all_configured_dice() -> None:
         configured_dice = {
             D4: range(1, 5),
             D6: range(1, 7),
@@ -37,53 +40,62 @@ def D1() -> Generator:
 
 
 class TestDieBuilderForMultiDie:
-    def test_creating_multi_die_by_multiplication(self, D1: DieBuilder) -> None:
+    @staticmethod
+    def test_creating_multi_die_by_multiplication(D1: DieBuilder) -> None:
         multi_die = D1 * 2
 
         assert multi_die() == (1, 1)
 
+    @staticmethod
     def test_creating_multi_die_by_multiplication_in_different_order(
-        self, D1: DieBuilder
+        D1: DieBuilder
     ) -> None:
         multi_die = 2 * D1
 
         assert multi_die() == (1, 1)
 
-    def test_creating_multi_die_by_addition(self, D1: DieBuilder) -> None:
+    @staticmethod
+    def test_creating_multi_die_by_addition(D1: DieBuilder) -> None:
         with pytest.raises(TypeError):
             1 + D1
 
+    @staticmethod
     def test_creating_multi_die_by_addition_in_different_order(
-        self, D1: DieBuilder
+        D1: DieBuilder
     ) -> None:
         with pytest.raises(TypeError):
             D1 + 1
 
+    @staticmethod
     def test_creating_multi_die_by_addition_with_another_die(
-        self, D1: DieBuilder
+        D1: DieBuilder
     ) -> None:
         multi_die = D1 + D1
 
         assert multi_die() == (1, 1)
 
+    @staticmethod
     def test_creating_multi_die_by_addition_with_another_dice(
-        self, D1: DieBuilder
+        D1: DieBuilder
     ) -> None:
         multi_die = D1 + D1 + D1
         assert multi_die() == (1, 1, 1)
 
+    @staticmethod
     def test_creating_multi_die_by_multiplication_with_another_die(
-        self, D1: DieBuilder
+        D1: DieBuilder
     ) -> None:
         with pytest.raises(TypeError):
             D1 * D1
 
-    def test_running_dice_with_multiplication_twice(self, D1: DieBuilder) -> None:
+    @staticmethod
+    def test_running_dice_with_multiplication_twice(D1: DieBuilder) -> None:
         multi_die = 3 * D6
         assert sum(multi_die()) >= 3 <= 6 * 3
         assert sum(multi_die()) >= 3 <= 6 * 3
 
-    def test_running_dice_with_addition_twice(self, D1: DieBuilder) -> None:
+    @staticmethod
+    def test_running_dice_with_addition_twice(D1: DieBuilder) -> None:
         multi_die = D6 + D6
         assert sum(multi_die()) >= 2 <= 6 * 2
         assert sum(multi_die()) >= 2 <= 6 * 2
